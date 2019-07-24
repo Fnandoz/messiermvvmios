2f98b4e34232c45d0f0ae358ec3ccc005376495c
Fernando Gomes <lfgsnando@gmail.com>
2019-07-23 23:39:51 -0300

    Initial Commit

---
+++ Hubbles Messier MVVM/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Hubbles Messier MVVM
+//
+//  Created by Fernando Gomes on 23/07/19.
+//  Copyright © 2019 Fernando Gomes. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
